373627f7d1a0f39b2c02e11725a3a2ec2feb706a
Lemonade Kidd <smejia03@gmail.com>
2020-03-27 17:19:34 -0500

    axios call complete

--- util/axios.js
+++ util/axios.js
@@ -1,17 +1,27 @@
 //axios - dotenv
 const axios = require("axios");
-//require(dotenv).config();
+require(dotenv).config();
 
 
 const axioS = { 
     //grab username from user input
     getUser(username)
  {   //begin axios get call
-
     return axios
     //utilizing GET call
     .get(
         //implementing the api link (along with the placeholder for the secret key)
-         'https://api.github.com/users/${username}?client_id=${ process.env.CLIENT_ID'
-    )}
+        `https://api.github.com/users/${username}?client_id=${
+            process.env.CLIENT_ID
+            }&client_secret=${process.env.CLIENT_SECRET}`
+    )
+    .catch(err => {
+        console.log(`User not found`);
+        process.exit(1);
+       
+    
+    });
 }
+}
+
+module.exports = axioS;
